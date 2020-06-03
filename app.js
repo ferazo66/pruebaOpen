32bf020d45913d8b3afdf93ee153bb4d213361a9
ferazo <ferazo@favorita.com>
2020-06-03 18:12:30 -0500

    Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello word!\n');
+});
+
+app.get('/enrique', function (req, res) {
+  res.send('Hello enrique!\n');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
+
